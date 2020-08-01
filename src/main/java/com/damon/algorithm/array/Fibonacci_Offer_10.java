a557f927618da8bf140616d4232023d6a1443b98
朱健 <zhujian@zhujiandeMacBook-Pro.local>
2020-08-01 14:33:54 +0800

    Update the Fibaonacci_Offer_10 when (a+b)%1000000007

--- src/main/java/com/damon/algorithm/array/Fibonacci_Offer_10.java
+++ src/main/java/com/damon/algorithm/array/Fibonacci_Offer_10.java
@@ -13,13 +13,13 @@ public class Fibonacci_Offer_10 {
             int first = 0;
             int second = 1;
             n = n - 1;
-            result = getFirstPlusSecond(first,second,n);
+            result = getFirstPlusSecond(first, second, n);
         }
         return result;
     }
 
     private int getFirstPlusSecond(int first, int second, int n) {
-        int result = first + second;
+        int result = (first + second) % 1000000007;
         n--;
         if (n > 0) {
             result = getFirstPlusSecond(second, result, n);
