e671d52e88ab1e82bf44f366cc6d6e3d4cc88af7
朱健 <zhujian@zhujiandeMacBook-Pro.local>
2020-08-30 16:23:00 +0800

    Add Offer 29

---
+++ src/main/java/com/damon/algorithm/array/SpiralOrder_Offer_29.java
@@ -0,0 +1,38 @@
+package com.damon.algorithm.array;
+
+public class SpiralOrder_Offer_29 {
+
+    public int[] spiralOrder(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return new int[0];
+        int rows = matrix.length, columns = matrix[0].length;
+        int left = 0, right = columns - 1;
+        int top = 0, bottom = rows - 1;
+
+        int[] result = new int[rows * columns];
+        int index = 0;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                result[index++] = matrix[top][i];
+            }
+            for (int j = top + 1; j <= bottom; j++) {
+                result[index++] = matrix[j][right];
+            }
+
+            if (left < right && top < bottom) {
+                for (int k = right - 1; k > left; k--) {
+                    result[index++] = matrix[bottom][k];
+                }
+                for (int v = bottom; v > top; v--) {
+                    result[index++] = matrix[v][left];
+                }
+            }
+
+            left++;
+            right--;
+            top++;
+            bottom--;
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/com/damon/algorithm/tree/SpiralOrder_Offer_29_Test.java
@@ -0,0 +1,35 @@
+package com.damon.algorithm.tree;
+
+import com.damon.algorithm.array.SpiralOrder_Offer_29;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SpiralOrder_Offer_29_Test {
+
+    private SpiralOrder_Offer_29 spiralOrder_offer_29;
+
+    @Before
+    public void before() {
+        this.spiralOrder_offer_29 = new SpiralOrder_Offer_29();
+    }
+
+    @Test
+    public void spiralOrderTest() {
+        int[][] test = new int[3][3];
+        int[] row1 = new int[]{1, 2, 3};
+        int[] row2 = new int[]{4, 5, 6};
+        int[] row3 = new int[]{7, 8, 9};
+
+        test[0] = row1;
+        test[1] = row2;
+        test[2] = row3;
+
+        int[] result = this.spiralOrder_offer_29.spiralOrder(test);
+        for (int i : result) {
+            System.out.println(i);
+        }
+        Assert.assertEquals(6, result[3]);
+    }
+
+}
