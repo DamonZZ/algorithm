0dcab0ac1414ad1cea0eb08e2344b54d28ceb0cd
朱健 <zhujian@zhujiandeMacBook-Pro.local>
2024-07-07 23:15:01 +0800

    Add maxSubArray using dynamic programming

---
+++ src/main/java/com/damon/algorithm/array/MaxSubArray_Hot100_53.java
@@ -0,0 +1,14 @@
+package com.damon.algorithm.array;
+
+public class MaxSubArray_Hot100_53 {
+
+    public int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+
+}
