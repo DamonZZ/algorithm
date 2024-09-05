08892f19042dbdd8f212e8dac3cff340fe3ff0e0
朱健 <zhujian@zhujiandeMacBook-Pro.local>
2024-09-05 23:17:55 +0800

    Add MaxProfit using greedy

---
+++ src/test/java/com/damon/algorithm/greedy/MaxProfit_Hot100_121_Test.java
@@ -0,0 +1,25 @@
+package com.damon.algorithm.greedy;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MaxProfit_Hot100_121_Test {
+
+    private MaxProfit_Hot100_121 maxProfit_hot100_121;
+
+    @Before
+    public void before() {
+        this.maxProfit_hot100_121 = new MaxProfit_Hot100_121();
+    }
+
+
+    @Test
+    public void findKthLargestTest() {
+        int[] nums = new int[]{7, 1, 5, 3, 6, 4};
+        int ans = this.maxProfit_hot100_121.maxProfit(nums);
+        Assert.assertEquals(5, ans);
+    }
+
+
+}
